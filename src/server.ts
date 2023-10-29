968407a542893a14a7d1f8f81faccfb73ac5e4cd
Maycon Silva <mayconslv.contato@gmail.com>
2023-10-29 15:54:13 -0300

    connecting to the database & create a query and mutation
    connecting to the DB  using Typeorm  and add a simple mutation to create a user and a query to search for users

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,52 @@
+import 'reflect-metadata'
 import { ApolloServer, gql } from 'apollo-server'
+import { AppDataSource } from './data-source'
+import { User } from './entities/User'
 
 const typeDefs = gql`
-  type Query {
-    hello: String!
-  }
+	type User {
+		id: ID!
+		name: String
+	}
+
+	type Query {
+		users: [User]
+	}
+
+	type Mutation {
+		create(name: String): User!
+	}
 `
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers: {
 		Query: {
-			hello: () => {
-				return 'Hello, world!'
+			async users() {
+				const repo = AppDataSource.getRepository(User)
+				const users = await repo.find()
+
+				return users
+			}
+		},
+		Mutation: {
+			create: async (_, args) => {
+				const repo = AppDataSource.getRepository(User)
+
+				const user = repo.create({
+					name: args.name
+				})
+				await repo.save(user)
+
+				return user
 			}
 		}
 	},
 })
 
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
+AppDataSource.initialize().then(() => {
+	console.log('Database OK.')
+	server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
+}).catch((err) => {
+	console.log('Error:', err)
+})
