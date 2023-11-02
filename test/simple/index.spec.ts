dd5e1d441c772665e9cd7db9b915a7db74feea69
Maycon Silva <mayconslv.contato@gmail.com>
2023-11-02 12:42:11 -0300

    doing  query with axios

--- test/simple/index.spec.ts
+++ test/simple/index.spec.ts
@@ -1,7 +1,20 @@
-import { describe, it } from 'mocha'
+import { before, describe, it } from 'mocha'
+import axios from 'axios'
 import assert from 'assert'
 
 describe('Simples test', () => {
+  before(async () => {
+    const queryResult = await axios.post('http://localhost:4000', {
+      query: `
+        query {
+          hello
+        }
+      `,
+    })
+    const { data } = queryResult.data
+    console.log(data)
+  })
+
   it('should return hello world', () => {
     const result = 'hello world'
     assert.equal(result, 'hello world')
