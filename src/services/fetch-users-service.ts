f7858b43547531ee84582a1326fabcf5f4755e07
Maycon Silva <112903637+MayconSlv@users.noreply.github.com>
2023-11-23 13:47:33 -0300

    add pagination in users query

--- src/services/fetch-users-service.ts
+++ src/services/fetch-users-service.ts
@@ -4,28 +4,41 @@ import { ResourceNotFoundError } from './errros/resource-not-found-error'
 
 interface FetchUsersServiceResponse {
   users: User[]
+  totalUsers: number
+  hasMoreAfter: boolean
+  hasMoreBefore: boolean
 }
 
 interface FetchUsersServiceRequest {
   quantity: number
+  page: number
 }
 
 export class FetchUsersService {
-  async execute({ quantity }: FetchUsersServiceRequest): Promise<FetchUsersServiceResponse> {
+  async execute({ quantity, page }: FetchUsersServiceRequest): Promise<FetchUsersServiceResponse> {
     const repo = AppDataSource.getRepository(User)
-    const users = await repo.find({
+    const databaseUsers = await repo.find({
       order: {
         name: 'ASC',
       },
       take: quantity,
     })
 
-    if (!users) {
+    const users = databaseUsers.slice((page - 1) * 10, page * 10)
+    const totalUsers = databaseUsers.length
+
+    const hasMoreAfter = page * 10 < totalUsers
+    const hasMoreBefore = page > 1
+
+    if (users.length === 0) {
       throw new ResourceNotFoundError()
     }
 
     return {
       users,
+      hasMoreAfter,
+      hasMoreBefore,
+      totalUsers,
     }
   }
 }
