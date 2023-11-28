f1e3b4f583a61ef37e1e16e3015dcb8f8fd310be
Maycon Silva <112903637+MayconSlv@users.noreply.github.com>
2023-11-28 12:15:28 -0300

    create Address entity

---
+++ src/entities/Address.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { User } from './User'
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  cep: string
+
+  @Column()
+  street: string
+
+  @Column()
+  streetNumber: string
+
+  @Column()
+  complement: string
+
+  @Column()
+  neighborhood: string
+
+  @Column()
+  city: string
+
+  @Column()
+  state: string
+
+  @ManyToOne(() => User, (user) => user.address)
+  user: User
+}
