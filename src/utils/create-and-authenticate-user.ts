29d662ac22955d465f03b6543f24dd89c3fb669f
Maycon Silva <112903637+MayconSlv@users.noreply.github.com>
2023-12-02 15:23:33 -0300

    add address to fake users

--- src/utils/create-and-authenticate-user.ts
+++ src/utils/create-and-authenticate-user.ts
@@ -2,16 +2,30 @@ import { hash } from 'bcryptjs'
 import { AppDataSource } from '../data-source'
 import { User } from '../entities/User'
 import { sign } from 'jsonwebtoken'
+import { Address } from '../entities/Address'
 
 export async function createAndAuthenticateUser() {
   const passwordHash = await hash('123456a', 6)
-  const repo = AppDataSource.getRepository(User)
 
-  const user = await repo.save({
+  const userRepository = AppDataSource.getRepository(User)
+  const addressRepository = AppDataSource.getRepository(Address)
+
+  const user = await userRepository.save({
     name: 'John Doe',
     email: 'johndoe@email.com',
     birthDate: '12-12-1990',
     passwordHash,
+  })
+
+  await addressRepository.save({
+    cep: '12345-678',
+    city: 'Javascript City',
+    complement: 'Typecript',
+    neighborhood: 'Java',
+    state: 'JS',
+    street: 'Javascript Street',
+    streetNumber: '123',
+    user,
   })
 
   const token = sign({ sub: user.id }, String(process.env.JWT_SECRET), { expiresIn: '1h' })
